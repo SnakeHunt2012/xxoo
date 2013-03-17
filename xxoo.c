abc54e17cc4e10b569e4ea273956f32311524208
SnakeHunt2012 <snakehunt2012@Linux.(none)>
2013-03-17 20:21:49 +0800

    scan number

--- xxoo.c
+++ xxoo.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX 1024 * 1024
 
+/* 
+ *  key: 0
+ *  dight: 1
+ *  string: 2
+ */
 struct token {
     int tag;
     int value;
@@ -20,19 +27,17 @@ main(int argc, char *argv[])
     char *prog = argv[0];
     char ori_code[MAX];
     char code[MAX];
-    /* begin test
     struct table token_table;
-
-    token_table.length = 1;
-    token_table.tokens[0] = (struct token *)malloc(sizeof(struct token));
-    token_table.tokens[0]->tag = 1;
-    token_table.tokens[0]->value = 10;
-    token_table.tokens[0]->str = "This is just a test\n";
-    end test */
 
     void load_code(FILE *, char *);
     void clean_code(char *, char *);
+    void scan_code(char *, struct table *token_table);
 
+    /* begin debug */
+    int i;
+    /* end debug */
+
+    token_table.length = 0;
     if (argc == 1)
 	fprintf(stderr, "%s: no input file\n", prog);
     else
@@ -45,13 +50,18 @@ main(int argc, char *argv[])
 	    fclose(fp);
 	    /* clean code */
 	    clean_code(ori_code, code);
+	    /* scan code */
+	    scan_code(code, &token_table);
+
 	    /* begin debug
-	    printf("test tag:%d\ntest value:%d\ntest str:%s",
-		   token_table.tokens[0]->tag,
-		   token_table.tokens[0]->value,
-		   token_table.tokens[0]->str
-		);
+	    for (i = 0; i < token_table.length; ++i)
+		printf("token[%d]:\n\ttag: %d\n\tvalue: %d\n\tstr:%s\n",
+		       i,
+		       token_table.tokens[i]->tag,
+		       token_table.tokens[i]->value,
+		       token_table.tokens[i]->str);
 	    end debug */
+
 	}
     if (ferror(stdout)) {
 	fprintf(stderr, "%s: error writing stdout\n", prog);
@@ -72,11 +82,10 @@ void load_code(FILE *ifp, char *ori_code)
 
 void clean_code(char *ori_code, char *code)
 {
-    int i, j, in_comment;
+    int i, j;
 
     i = 0;
     j = 0;
-    in_comment = 0;
 
     while (ori_code[i] != '\0') {
 	/* remove comment */
@@ -89,4 +98,45 @@ void clean_code(char *ori_code, char *code)
 	code[j++] = ori_code[i++];
     }
     code[j] = '\0';
+}
+
+void scan_code(char *code, struct table *token_table)
+{
+    int i, line, v;
+
+    i = 0;
+    line = 0;
+    while (code[i]) {
+	/* ignore blank character */
+	while (code[i]) {
+	    if (code[i] == ' ' || code[i] == '\t') {
+		i++;
+		continue;
+	    } else if (code[i] == '\n') {
+		i++;
+		line++;
+	    }
+	    else
+		break;
+	}
+	/* scan number */
+	if (isdigit(code[i])) {
+	    v = 0;
+	    do {
+		v = 10 * v + (int)(code[i]) - 48;
+		i++;
+	    } while (isdigit(code[i]));
+	    printf("outer\n");
+	    token_table->tokens[token_table->length] = 
+		(struct token *)malloc(sizeof(struct token));
+	    token_table->tokens[token_table->length]->tag = 1;
+	    token_table->tokens[token_table->length]->value = v;
+	    token_table->tokens[token_table->length]->str = NULL;
+	    token_table->length++;
+	}
+	/* scan string */
+	else if (isalpha(code[i])) {
+	    i++;
+	} else i++;
+    }
 }
