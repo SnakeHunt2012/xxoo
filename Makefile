f594c984c6bf24cf4052d92f469f48d9ca5d51d5
SnakeHunt2012 <snakehunt2012@Linux.(none)>
2013-04-11 20:03:38 +0800

    Add test in Makefile and reorganise files.

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 SHELL = /bin/sh
 
+all : xxoo
+
 xxoo : xxoo.c lex/lex.c parse/parse.c
 	cc -o xxoo xxoo.c lex/lex.c parse/parse.c
+
+test : xxoo test/test.c
+	./xxoo test/test.c
 
 clean :
 	rm xxoo
---
+++ test/test.c
@@ -0,0 +1,12 @@
+int main ( )
+{
+    int a = 10 ;
+    double b = 123.456 ;
+    double c = 321.654 ;
+    char c = 'x' ;
+    string d = "abc    def" ;
+
+    b = c + b ;
+    b += b ;
+    b ++ ;
+}
